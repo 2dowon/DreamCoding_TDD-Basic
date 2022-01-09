3620e55dc5eedb3819a2aebcd9b24014136c7dd0
2dowon <dwon424@gmail.com>
2022-01-09 23:16:46 +0900

    ✅  test for calculator

---
+++ src/test/calculator.test.js
@@ -0,0 +1,60 @@
+const Calculator = require("../calculator.js");
+
+describe("Calculator", () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it("inits with 0", () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it("sets", () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
+
+  it("clear", () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it("add => 2 + 3 = 5", () => {
+    cal.set(2);
+    cal.add(3);
+    expect(cal.value).toBe(5);
+  });
+
+  it("subtract => 5 - 2 = 3", () => {
+    cal.set(5);
+    cal.subtract(2);
+    expect(cal.value).toBe(3);
+  });
+
+  it("multiply => 5 * 2 = 10", () => {
+    cal.set(5);
+    cal.multiply(2);
+    expect(cal.value).toBe(10);
+  });
+
+  describe("divide", () => {
+    it("0 / 0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it("4 / 4 === 1", () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
+});
